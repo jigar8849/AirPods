52ab06fb783239960de15f52bb373b95b15d48c5
jigar <jp0001296@gmail.com>
2024-11-20 20:34:23 +0530

    r

--- main.js
+++ main.js
@@ -29,3 +29,39 @@ function scrollHeader(){
 window.addEventListener("scroll", scrollHeader); 
 
 // change background header end
+
+// scroling Section start
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      console.log(`Scrolling to section: ${sectionId}`);
+      section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.log(`Section not found: ${sectionId}`);
+    }
+  }
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const navLinks=document.querySelectorAll('.li');
+    navLinks.forEach(link => {
+        link.addEventListener("click",()=>{
+            toggleNav();
+        });
+    });
+
+
+    document.getElementById('nav-1').addEventListener('click', () => scrollToSection('ts'));
+    document.getElementById('nav-2').addEventListener('click', () => scrollToSection('ov'));
+    document.getElementById('nav-3').addEventListener('click', () => scrollToSection('cp'));
+});
+
+    // document.getElementById('nav-1').addEventListener('click',()=> scrollToSection('ts'));
+    // document.getElementById('nav-2').addEventListener('click',()=> scrollToSection('ov'));
+    // document.getElementById('nav-3').addEventListener('click',()=> scrollToSection('cp'));
+
+
+
+// scroling Section end
